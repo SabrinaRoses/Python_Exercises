34db2ac385bb33cece4dfaf621616ebd6ad6c4a0
SabrinaRoses <65089744+SabrinaRoses@users.noreply.github.com>
2022-10-27 20:15:32 +0100

    Delete Formatted_Name directory

--- Formatted_Name/name_function.py
+++
@@ -1,9 +0,0 @@
-#Função simples que aceita o primeiro e ultimo nome 
-#Devolve um nome completo e formatado de modo elegante
-
-def get_fomarted_name(first, last):
-    """gera um nome completo formatado de modo elegante"""
-    full_name = first + ' ' + last
-    return  full_name.title()
-
-#criar um outro programa que use a função para fazer o teste
